37fbccc6e1c8692a6ac261ae931abfbf548a2052
BW5442 <valentin.scanu@engie.com>
2022-03-30 16:37:53 +0200

    Optimize p_010

--- src/p_010.rs
+++ src/p_010.rs
@@ -1,16 +1,7 @@
-fn is_prime(n: i64) -> bool {
-    let n_int_sqrt = (n as f64).sqrt() as i64;
+use crate::common::sieve_of_eratosthenes;
 
-    for i in (3..=n_int_sqrt).step_by(2) {
-        if n % i == 0 {
-            return false;
-        }
-    }
-    true
-}
 
 #[allow(dead_code)]
 pub fn problem_010() -> i64 {
-    let s: i64 = (11..2_000_000).step_by(2).filter(|n| is_prime(*n)).sum();
-    2 + 3 + 5 + 7 + s
+    sieve_of_eratosthenes(2_000_000).iter().sum()
 }
